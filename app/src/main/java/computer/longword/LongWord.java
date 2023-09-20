1d0a2a8c381eb1a08dc93ac030922e607ca9e8b2
Joel Bengs <joel.bengs@gmail.com>
2023-09-20 19:56:35 +0200

    Longword copy, work in progress

--- app/src/main/java/computer/longword/LongWord.java
+++ app/src/main/java/computer/longword/LongWord.java
@@ -1,6 +1,8 @@
 package computer.longword;
 
 import computer.Word;
+import exceptions.WordMismatchException;
+import exceptions.*;
 
 public class LongWord extends Word{
     private long l;
@@ -9,7 +11,9 @@ public class LongWord extends Word{
         
     }
 
-    public void add(Word other, Word result) {}
+    public void add(Word other, Word result) {
+        result.copy(this.getWord(null) + other.getWord(null));
+    }
 
     public void mul(Word other, Word result) {}
 
@@ -17,7 +21,18 @@ public class LongWord extends Word{
 
     public void print() {}
 
-    public void copy(Word other) {
-
+    public static void copyStatic(Word source, Word destination) {
+        if(source instanceof LongWord && destination instanceof LongWord){
+            ((LongWord) destination).l = ((LongWord) source).l;
+        }
     }
+
+     public void copy(Word other) {
+        if(!(other instanceof LongWord)){
+            throw new WordMismatchException();
+        }
+            this.l = ((LongWord) other).l;
+        }
+    }
+
 }
