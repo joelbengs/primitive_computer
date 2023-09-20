3c8b6d7da160250ce93cb966e16a6773260ea91e
Henrik Vester <henrik645@gmail.com>
2023-09-20 20:16:06 +0200

    Implement LongWordFactory

--- app/src/main/java/computer/longword/LongWord.java
+++ app/src/main/java/computer/longword/LongWord.java
@@ -1,36 +1,36 @@
 package computer.longword;
 
 import computer.Word;
-import exceptions.WordMismatchException;
 import exceptions.*;
+import exceptions.WordMismatchException;
 
-public class LongWord extends Word{
+public class LongWord extends Word {
     private long l;
 
-    public LongWord(String s) {
-        
+    public LongWord(long l) {
+        this.l = l;
     }
 
     public void add(Word other, Word result) {
-        if(!(other instanceof LongWord && result instanceof LongWord)){
+        if (!(other instanceof LongWord && result instanceof LongWord)) {
             throw new WordMismatchException();
         }
-        ((LongWord) result).l = (this.l + ((LongWord)other).l);
+        ((LongWord) result).l = (this.l + ((LongWord) other).l);
     }
-    
 
     public void mul(Word other, Word result) {
-        if (!(other instanceof LongWord && result instanceof LongWord)){
+        if (!(other instanceof LongWord && result instanceof LongWord)) {
             throw new WordMismatchException();
         }
-        ((LongWord) result).l = (this.l * ((LongWord)other).l);
+        ((LongWord) result).l = (this.l * ((LongWord) other).l);
     }
+
     @Override
     public boolean equals(Word other) {
-        if (!(other instanceof LongWord)){
+        if (!(other instanceof LongWord)) {
             throw new WordMismatchException();
-        }else{
-        return (((LongWord) other).l == this.l);
+        } else {
+            return (((LongWord) other).l == this.l);
         }
     }
 
@@ -39,17 +39,15 @@ public class LongWord extends Word{
     }
 
     public static void copyStatic(Word source, Word destination) {
-        if(source instanceof LongWord && destination instanceof LongWord){
+        if (source instanceof LongWord && destination instanceof LongWord) {
             ((LongWord) destination).l = ((LongWord) source).l;
         }
     }
 
-     public void copy(Word other) {
-        if(!(other instanceof LongWord)){
+    public void copy(Word other) {
+        if (!(other instanceof LongWord)) {
             throw new WordMismatchException();
         }
-            this.l = ((LongWord) other).l;
-        }
-    
-
+        this.l = ((LongWord) other).l;
+    }
 }
