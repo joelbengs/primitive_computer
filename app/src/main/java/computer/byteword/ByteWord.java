00747a68e2d239ca1c6f52d3bbefbc4e4b64793f
Henrik Vester <henrik645@gmail.com>
2023-09-20 20:04:38 +0200

    Implement ByteWord and ByteWordFactory

--- app/src/main/java/computer/byteword/ByteWord.java
+++ app/src/main/java/computer/byteword/ByteWord.java
@@ -1,6 +1,50 @@
 package computer.byteword;
 
-public class ByteWord {
+import java.util.function.BinaryOperator;
+import computer.Word;
+import exceptions.WordMismatchException;
+
+public class ByteWord extends Word {
     private byte b;
 
+    public ByteWord(Byte b) {
+        this.b = b;
+    }
+
+    private void calculate(Word other, Word result, BinaryOperator<Byte> operator) {
+        if (!(other instanceof ByteWord) || !(result instanceof ByteWord)) {
+            throw new WordMismatchException();
+        }
+        var otherByteWord = (ByteWord) other;
+        var resultByteWord = (ByteWord) result;
+        resultByteWord.b = operator.apply(otherByteWord.b,this.b);
+    }
+
+    @Override
+    public void add(Word other, Word result) {
+        calculate(other, result, (x, y) -> (byte) (x + y));
+    }
+
+    @Override
+    public void mul(Word other, Word result) {
+        calculate(other, result, (x, y) -> (byte) (x * y));
+    }
+
+    @Override
+    public void print() {
+        System.out.println(b);
+    }
+
+    @Override
+    public boolean equals(Word other) {
+        if (!(other instanceof ByteWord)) {
+            throw new WordMismatchException();
+        }
+        var otherByteWord = (ByteWord) other;
+        return this.b == otherByteWord.b;
+    }
+
+    public void copy(Word source) {
+        this.b = ((ByteWord) source).b;
+    }
 }
--- app/src/main/java/computer/byteword/ByteWordFactory.java
+++ app/src/main/java/computer/byteword/ByteWordFactory.java
@@ -1,14 +1,15 @@
 package computer.byteword;
 
-import computer.Word;
 import computer.WordFactory;
 
 public class ByteWordFactory implements WordFactory {
 
-    @Override
-    public Word word(String value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'word'");
+    public ByteWord word(String s) {
+        try {
+            byte b = Byte.parseByte(s);
+            return new ByteWord(b);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
-    
 }
