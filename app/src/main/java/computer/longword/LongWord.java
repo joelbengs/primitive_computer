b848ba2f1f5766b33b92d874820f210a9c8c7353
Joel Bengs <joel.bengs@gmail.com>
2023-09-20 19:10:54 +0200

    Longword skeleton!

--- app/src/main/java/computer/longword/LongWord.java
+++ app/src/main/java/computer/longword/LongWord.java
@@ -1,6 +1,19 @@
 package computer.longword;
 
-public class LongWord {
+import computer.Word;
+
+public class LongWord extends Word{
     private long l;
 
+    public LongWord(String s) {}
+
+    public void add(Word w1, Word w2) {}
+
+    public void mull(Word w1, Word w2) {}
+
+    public boolean equals(Word w) {}
+
+    public void print() {}
+    
+    public void copy(Word w) {}
 }
