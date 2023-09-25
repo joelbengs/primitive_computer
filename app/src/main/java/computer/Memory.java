e7b0a6331faacebe5a7b464bf6ab80a14360de9d
Henrik Vester <henrik645@gmail.com>
2023-09-25 09:30:40 +0200

    Format code

--- app/src/main/java/computer/Memory.java
+++ app/src/main/java/computer/Memory.java
@@ -1,18 +1,18 @@
 package computer;
 
 public class Memory {
-private Word[] memoryList;
-private WordFactory factory;
+    private Word[] memoryList;
+    private WordFactory factory;
 
     public Memory(int size, WordFactory factory) {
         this.factory = factory;
         this.memoryList = new Word[size];
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             memoryList[i] = this.factory.defaultWord();
         }
     }
 
-    public Word wordAt(int position){
+    public Word wordAt(int position) {
         return memoryList[position];
     }
 }
