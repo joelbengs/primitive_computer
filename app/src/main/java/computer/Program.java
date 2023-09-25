69e720d391a0128e0de8865caee0aa03e58d8d19
Henrik Vester <henrik645@gmail.com>
2023-09-25 09:57:49 +0200

    Add Program class

--- app/src/main/java/computer/Program.java
+++ app/src/main/java/computer/Program.java
@@ -1,3 +1,17 @@
 package computer;
 
-public class Program {}
+import instructions.Instruction;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Program {
+    private List<Instruction> instructions;
+
+    public Program() {
+        this.instructions = new ArrayList<>();
+    }
+
+    public void add(Instruction instruction) {
+        this.instructions.add(instruction);
+    }
+}
