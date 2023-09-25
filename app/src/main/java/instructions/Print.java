76b3e67c6be8f9a60a76b6b95e846b8e772434a7
Henrik Vester <henrik645@gmail.com>
2023-09-25 09:30:44 +0200

    Add Print

--- app/src/main/java/instructions/Print.java
+++ app/src/main/java/instructions/Print.java
@@ -1,3 +1,19 @@
 package instructions;
 
-public class Print {}
+import computer.InstructionCounter;
+import computer.Memory;
+import computer.Operand;
+
+public class Print implements Instruction {
+
+    private Operand op;
+
+    public Print(Operand op) {
+        this.op = op;
+    }
+
+    public void execute(Memory memory, InstructionCounter instructionCounter) {
+        var word = op.getWord(memory);
+        System.out.println(word);
+    }
+}
